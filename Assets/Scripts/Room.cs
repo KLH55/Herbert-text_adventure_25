810c3c729010d8a505c544a292456c1820a652f3
Kris Herbert <klh55@pct.edu>
2025-02-24 15:16:11 -0500

    Class part 1

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Text/Room")]
+public class Room : ScriptableObject
+{
+    public string rooomName;
+    [TextArea]
+    public string description;
+    public Exit[] exits;
+
+    public bool hasKey;
+    public bool hasOrb;
+
+
+}
